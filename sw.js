1a6f6182595e619375ca7ca3393b4cefa2ba57d7
jonFernandez6 <135345883+jonFernandez6@users.noreply.github.com>
2025-09-06 16:05:58 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'aprendizaje-ultra-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './sw.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
+});
